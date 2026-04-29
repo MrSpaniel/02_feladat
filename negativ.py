48672651c08ce7c9927e300202516ef441fdbac9
Fehér Dániel <feher.daniel.sandor@diak.szbi-pg.hu>
2026-04-29 08:15:50 +0200

    Update negativ.py

--- negativ.py
+++ negativ.py
@@ -2,10 +2,14 @@ import random
 
 pozitiv = []
 negativ = []
-szamok = [random.randint(-51, 51) for i in range(101)]
+szamok = [random.randint(-50, 50) for i in range(100)]
 
 for i in szamok:
     if i < 0:
         negativ.append(i)
     else:
         pozitiv.append(i)
+
+y = len(negativ)
+
+print(f'A generált számok között {y} negatív szerepelt.')
